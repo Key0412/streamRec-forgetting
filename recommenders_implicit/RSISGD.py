22ffa3f4770e6a999633239abbb5302c85be6a96
Klismam Franciosi Pereira <klismam.f.pereira@inesctec.pt>
2022-05-23 10:53:10 +0100

    add small description RSISGD

--- recommenders_implicit/RSISGD.py
+++ recommenders_implicit/RSISGD.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class RSISGD(ISGD):
     """
-    
+    ISGD with random negative sampling
     """
     def __init__(self, data: ImplicitData, num_factors: int = 10, num_iterations: int = 10, learn_rate: float = 0.01, u_regularization: float = 0.1, i_regularization: float = 0.1, random_seed: int = 1, ra_length: int = 1):
         super().__init__(data, num_factors, num_iterations, learn_rate, u_regularization, i_regularization, random_seed)
@@ -30,4 +30,3 @@ class RSISGD(ISGD):
                 self._UpdateFactors(user_id, negative_item_id, True, False, 0)
         
         self._UpdateFactors(user_id, item_id)
-        
