c33e1109c45c96718f44a420fcd4bb009c90fa3a
Klismam u2004 <kp.franciosi@gmail.com>
2023-03-01 11:37:51 +0000

    update requirements and kernel to u2004

--- eval_implicit/__init__.py
+++ eval_implicit/__init__.py
@@ -3,4 +3,3 @@ from .EvalLeaveLastOut import EvalLeaveLastOut
 from .EvalHoldout import EvalHoldout 
 from .EvaluateAndStore import EvaluateAndStore
 from .EvaluateHoldouts import EvaluateHoldouts
-from .EvaluateHoldoutsFlurs import EvaluateHoldoutsFlurs
