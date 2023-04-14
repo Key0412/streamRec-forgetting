224a7dd40473cfacaca386a556dac06f106102ad
Klismam u2004 <kp.franciosi@gmail.com>
2023-04-14 17:47:02 +0100

    separate function to get samples from the run f

--- elliot_experiments/elliot_movielens/multivae/run_multivae_experiment.py
+++ elliot_experiments/elliot_movielens/multivae/run_multivae_experiment.py
@@ -1,7 +1,6 @@
 from sys import path
 path.insert(0, '/home/kpfra/streamRec-forgetting/elliot_experiments')
-from datetime import datetime
-from source import experiment, data_processing
+from source import experiment
 
 '''
 To run an experiment, provide:
@@ -11,17 +10,6 @@ To run an experiment, provide:
 
 path_to_config_file = "/home/kpfra/streamRec-forgetting/elliot_experiments/elliot_movielens/multivae/ml_multivae_b0_h0_config.yml"
 path_to_datasets = '/home/kpfra/streamRec-forgetting/elliot_experiments/elliot_movielens/datasets'
-path_to_original_df = '/home/kpfra/streamRec-forgetting/notebooks/output/movielens_dump/sampled_movielens.csv'
-
-# get sampled data
-# data_processing.process_sample(
-#     data_path=path_to_original_df,
-#     path_to_datasets=path_to_datasets,
-#     user_col='UserID',
-#     item_col='ItemID',
-#     date_conversion_function=lambda x: datetime.strptime(x, '%Y-%m-%d %X'),
-#     test_run=True
-#     )
 
 # Training/Evaluation
 experiment.run(path_to_config_file, path_to_datasets)
